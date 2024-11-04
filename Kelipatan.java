d08a7da41c6e41a486e79cd9c584f2a964ea3f32
NaufalArif2095 <naufalarif200905@gmail.com>
2024-11-04 15:57:17 +0700

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PBO1;
+
+/*
+ * @author
+ * NAMA    : Naufal Arif Maulana
+ * KELAS   : PBO12
+ * NIM     : 23176026
+ * Deskripsi Program : Program ini berisi program untuk menampilkan 
+ *  bilangan kelipatan 3,5 sampai dengan 35.
+ */
+
+public class Kelipatan {
+    public static void main(String[] args) {
+        System.out.println("LATIHAN 18. KELIPATAN");
+
+        // Looping from 3.5 to 35 in multiples of 3.5
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
+
